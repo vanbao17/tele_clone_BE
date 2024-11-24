1525afb9c945ccdd497f36fdc5fe15213d242986
Truc Pham <kientruc582@gmail.com>
2024-11-24 16:13:00 +0700

    feat: forgot password, send mail, reset

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\ResetPasswordController;
 
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/check-email', [RegisterController::class, 'checkEmail']);
+Route::post('forgot-password', [ResetPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.reset');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
